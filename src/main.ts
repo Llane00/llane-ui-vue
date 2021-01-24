f05fe7f6653965550450dc5d0b229d4ff0b9c9db
Llane00 <bluetinestyle@126.com>
2021-01-24 20:58:42 +0800

    feat: 修改createRouter变量

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import {createWebHashHistory, createRouter} from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
+
 import test from './components/test.vue'
+import aboutMe from './components/aboutMe.vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+const routes = [
+  { path: '/', component: test }, 
+  { path: '/HelloWorld', component: HelloWorld }, 
+  { path: '/aboutMe', component: aboutMe }
+];
 
 const history = createWebHashHistory();
 const router = createRouter({
-  history: history,
-  routes: [{
-    path: '/', component: test
-  }, {
-    path: '/HelloWorld', component: HelloWorld
-  }]
+  history,
+  routes
 });
 
 const app = createApp(App)
